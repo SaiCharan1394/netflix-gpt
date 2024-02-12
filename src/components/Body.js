a703d643c27bf06c8650ac631232ba3a8a63a26f
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2024-02-12 23:00:31 +0530

    deploy in firebase

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import Browse from "./Browse";
-import Header from "./Header";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
 const Body = () => {
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,5 @@
 export const checkValidData = (email, password) => {
-  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
   if (!isEmailValid) return "Email Id not correct";
