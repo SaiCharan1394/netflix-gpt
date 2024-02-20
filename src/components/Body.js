e7b91d7e370c98ebb35ca7065dd8cca7f2b4ff52
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2024-02-20 16:13:45 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,8 +4,11 @@ import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
-
+import { auth } from "../utils/firebase";
+import { addUser,removeUser } from "../utils/userSlice";
+import { UseDispatch, useDispatch } from "react-redux";
 const Body = () => {
+  const dispatch=useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -16,13 +19,10 @@ const Body = () => {
   useEffect(()=>{
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        // ...
+        const{ uid, email, displayName}= user;
+        dispatch(addUser({uid:uid, email:email, displayName:displayName}))
       } else {
-        // User is signed out
-        // ...
+        dispatch(removeUser())
       }
     });
   },[])
