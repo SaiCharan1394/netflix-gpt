cad6ec5586f2ad9688bb492c95aad1a0856ff47f
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2024-02-25 01:23:33 +0530

    .

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import React, { useEffect } from "react";
+import React from "react";
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { addUser,removeUser } from "../utils/userSlice";
+
 import {  useDispatch } from "react-redux";
 const Body = () => {
-  const dispatch=useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -16,16 +13,7 @@ const Body = () => {
     },
     { path: "/browse", element: <Browse /> },
   ]);
-  useEffect(()=>{
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const{ uid, email, displayName, photoUrl}= user;
-        dispatch(addUser({uid:uid, email:email, displayName:displayName, photoUrl:photoUrl}))
-      } else {
-        dispatch(removeUser())
-      }
-    });
-  },[])
+  
   return (
     <div>
       <RouterProvider router={appRouter}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserDropDown from "./UserDropDown";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { addUser,removeUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const user = useSelector((store) => store.user);
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const{ uid, email, displayName, photoUrl}= user;
+        dispatch(addUser({uid:uid, email:email, displayName:displayName, photoUrl:photoUrl}))
+        navigate("/browse")
+      } else {
+        dispatch(removeUser())
+        navigate("/")
+      }
+    });
+  },[])
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
